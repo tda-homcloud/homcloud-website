d6d10b509d5b8884cd939fc08cf97ca8f46cff26
Ippei Obayashi <ohai@kmc.gr.jp>
2021-04-13 09:58:03 +0900

    Update

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ rm -r conda-channel
 mkdir conda-channel
 cp -r conda-files/linux-64 conda-channel
 cp -r conda-files/win-64 conda-channel
+cp -r conda-files/osx-64 conda-channel
 eval "$(/home/ohai/miniconda3/bin/conda shell.bash hook)"
 conda index conda-channel
