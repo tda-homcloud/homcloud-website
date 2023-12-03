67a18d028a7a9d512173bf03db0403409265b92a
Ippei Obayashi <ohai@kmc.gr.jp>
2023-12-03 23:04:51 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-PYTHON_API_DIR="../main/python/apidoc-src/_build/html"
-
 gsutil -m rsync -r build gs://homcloud.dev
-gsutil -m rsync -r $PYTHON_API_DIR gs://homcloud.dev/python-api
 gsutil -m rsync -r conda-channel gs://homcloud.dev/conda-channel
 # gcloud compute url-maps invalidate-cdn-cache homcloud-dev-lb --project homcloud-website  --host homcloud.dev --path "/*" 
